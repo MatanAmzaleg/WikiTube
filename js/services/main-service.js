213cef9b52ced610ebda1f9d197a8557908ef55a
Matan Amzaleg <91034431+UnderDoggy7@users.noreply.github.com>
2022-11-01 12:40:54 +0200

    Add files via upload

--- js/services/main-service.js
+++ js/services/main-service.js
@@ -2,7 +2,21 @@
 
 const SEARCH_KEY = 'searchWord'
 const STORAGE_KEY = 'userCache'
-const gUserCache = loadFromStorage(STORAGE_KEY) || {}
+const gUserCache = loadFromStorage(STORAGE_KEY) || [{
+    title: "David Guetta &amp; Bebe Rexha - I&#39;m Good (Blue) [Official Music Video]",
+    videoId: "90RLzVUuXe4",
+    videoImg: "https://i.ytimg.com/vi/90RLzVUuXe4/default.jpg"
+},
+{
+    title: "David Guetta | United at Home - Dubai Edition",
+    videoId: "xaurMcGqZHU",
+    videoImg: "https://i.ytimg.com/vi/xaurMcGqZHU/default.jpg"
+},
+{
+    title: "David Guetta LIVE @ Ultra Music Festival Miami 2022",
+videoId: "zFIikcWbFRc",
+videoImg: "https://i.ytimg.com/vi/zFIikcWbFRc/default.jpg"
+}]
 let gCurrValue = loadFromStorage(SEARCH_KEY) || 'calvinharris'
 let gCacheMode = false
 
@@ -15,43 +29,43 @@ const wikiUrl = `https://en.wikipedia.org/w/api.php?&origin=*&action=query&list=
 getWiki()
 
 
-function updateSearch(val){
+function updateSearch(val) {
     gCurrValue = val
     saveToStorage(SEARCH_KEY, gCurrValue)
 }
 
-function getWiki(){
+function getWiki() {
     return fetch(wikiUrl)
-    .then(res => res.json())
-    .then(res=> res.query.search)
-    .then(res=> {return res[0]})
+        .then(res => res.json())
+        .then(res => res.query.search)
+        .then(res => { return res[0] })
 }
 
-function isCacheMode(){
+function isCacheMode() {
     return gCacheMode
 }
 
 
-function getVideos(){
-    if(gUserCache[0].title.toLowerCase().includes(gCurrValue.toLowerCase())){
+function getVideos() {
+    if (gUserCache[0].title.toLowerCase().includes(gCurrValue.toLowerCase())) {
         console.log('retrieving from cache');
         gCacheMode = true
         return loadFromStorage(STORAGE_KEY)
-    }else {
+    } else {
         return fetch(youtubeUrl)
-    .then(res=> res.json())
-    .then(videos => {
-        const videosPrms = videos.items.map(video => {
-            return{
-                videoId:video.id.videoId,
-                videoImg: video.snippet.thumbnails.default.url,
-                title: video.snippet.title,
-            }
-        })
-        saveToStorage(STORAGE_KEY, videosPrms)
-        return Promise.all(videosPrms)
-     })
-    }  
-    
+            .then(res => res.json())
+            .then(videos => {
+                const videosPrms = videos.items.map(video => {
+                    return {
+                        videoId: video.id.videoId,
+                        videoImg: video.snippet.thumbnails.default.url,
+                        title: video.snippet.title,
+                    }
+                })
+                saveToStorage(STORAGE_KEY, videosPrms)
+                return Promise.all(videosPrms)
+            })
+    }
+
 
 }
